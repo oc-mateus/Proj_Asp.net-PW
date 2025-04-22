404f54c8422a1416be2a9ab17468cbdd22362d71
Kayo Henrique <230232@LTI5401>
2025-04-22 18:07:47 -0300

    Att Feitas em Aula.

--- WebApplication1/Controllers/AlunoController.cs
+++ WebApplication1/Controllers/AlunoController.cs
@@ -1,22 +1,31 @@
 ﻿using System;
+
 using System.Collections.Generic;
-using System.IO;
+
 using System.Linq;
+
 using System.Web;
+
 using System.Web.Mvc;
-using iTextSharp.text;
-using iTextSharp.text.pdf;
+
 using WebApplication1.Models;
+
+using System.IO;
+
+using iTextSharp.text;
+
+using iTextSharp.text.pdf;
+
+using OfficeOpenXml;
+
+using System.Drawing;
 
 namespace WebApplication1.Controllers
 {
     public class AlunoController : Controller
     {
         // GET: Aluno
-        public ActionResult Index()
-        {
-            return View();
-        }
+        public ActionResult Index() => RedirectToAction("Listar");
 
         public ActionResult Listar()
         {
@@ -37,65 +46,113 @@ namespace WebApplication1.Controllers
         [HttpPost]
         [ValidateAntiForgeryToken]
 
-        public ActionResult Delete(int id, Aluno aluno)
+        public ActionResult DeleteAjax(int id)
         {
-            Aluno.Procurar(Session, id)?.Excluir(Session);
-
-            return RedirectToAction("Listar");
+            var aluno = Aluno.Procurar(Session, id);
+            if (aluno != null)
+            {
+                aluno.Excluir(Session);
+                return Json(new { sucesso = true });
+            }
+            return new HttpStatusCodeResult(404, "Aluno não encontrado");
         }
 
 
         public ActionResult Edit(int id)
         {
-            return View((Session["ListaAluno"] as List<Aluno>).ElementAt(id));
+            return View(Aluno.Procurar(Session, id));
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Edit(int id, Aluno aluno)
         {
-            var listaAluno = Session["ListaAluno"] as List<Aluno>;
-            if (listaAluno == null)
+            if (ModelState.IsValid)
             {
+                aluno.Editar(Session, id);
                 return RedirectToAction("Listar");
             }
-
-            Aluno alunoExistente = listaAluno.ElementAtOrDefault(id);
-            if (alunoExistente == null)
-            {
-                return RedirectToAction("Listar");
-            }
-
-            alunoExistente.Nome = aluno.Nome;
-            alunoExistente.RA = aluno.RA;
-            alunoExistente.Nascimento = aluno.Nascimento;
-           
-
-            
-
-            Session["ListaAluno"] = listaAluno;
-
-            return RedirectToAction("Listar");
+            return View(aluno);
         }
 
-        public ActionResult Create()
-        {
-            return View(new Aluno());
-        }
+        public ActionResult Create() => View(new Aluno());
 
         [HttpPost]
         [ValidateAntiForgeryToken]
 
         public ActionResult Create(Aluno aluno)
         {
-            aluno.Adicionar(Session);
+            if (ModelState.IsValid)
+            {
+                aluno.Adicionar(Session);
+                return RedirectToAction("Listar");
+            }
+            return View(aluno);
+        }
+        public ActionResult DownloadExcel()
 
-            return RedirectToAction("Listar");
+        {
+
+            var lista = Session["ListaAluno"] as List<Aluno>;
+
+            if (lista == null || !lista.Any())
+
+                return RedirectToAction("Listar");
+
+            ExcelPackage.License.SetNonCommercialOrganization("Terrier Corp.");
+            using (var pacote = new ExcelPackage())
+
+            {
+
+                var planilha = pacote.Workbook.Worksheets.Add("Alunos");
+
+                planilha.Cells[1, 1].Value = "Nome";
+
+                planilha.Cells[1, 2].Value = "Email";
+
+                planilha.Cells[1, 3].Value = "RA";
+
+                planilha.Cells[1, 4].Value = "Data de Nascimento";
+
+
+                planilha.Row(1).Style.Font.Bold = true;
+
+                planilha.Row(1).Style.Fill.PatternType = OfficeOpenXml.Style.ExcelFillStyle.Solid;
+
+                planilha.Row(1).Style.Fill.BackgroundColor.SetColor(Color.LightGray);
+
+
+                for (int i = 0; i < lista.Count; i++)
+
+                {
+
+                    var aluno = lista[i];
+
+                    planilha.Cells[i + 2, 1].Value = aluno.Nome;
+
+                    planilha.Cells[i + 2, 2].Value = aluno.Email;
+
+                    planilha.Cells[i + 2, 3].Value = aluno.RA;
+
+                    planilha.Cells[i + 2, 4].Value = aluno.Nascimento.ToShortDateString();
+
+                }
+
+
+                planilha.Cells.AutoFitColumns();
+
+                return File(pacote.GetAsByteArray(), "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "Alunos.xlsx");
+
+            }
+
         }
 
-        
-
     }
+
+
+
+
+
 
 
 }
--- WebApplication1/Models/Aluno.cs
+++ WebApplication1/Models/Aluno.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Ajax.Utilities;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,8 +9,23 @@ namespace WebApplication1.Models
 {
     public class Aluno
     {
+
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O nome é Obrigatório!")]
         public string Nome { get; set; }
+        [Required(ErrorMessage = "O e-mail é Obrigatório!")]
+        [EmailAddress(ErrorMessage = "E-mail inválido!")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "O RA é Obrigatório!")]
         public string RA { get; set; }
+
+        [Required(ErrorMessage = "A data de nascimento é obrigatória.")]
+
+        [DataType(DataType.Date)]
+
+        [Display(Name = "Data de Nascimento")]
         public DateTime Nascimento { get; set; }
         public string GetData()
         {
@@ -18,69 +34,63 @@ namespace WebApplication1.Models
 
         public static void GerarLista(HttpSessionStateBase session)
         {
-            if (session["ListaAluno"] != null)
+            if (session["ListaAluno"] == null)
             {
-                if (((List<Aluno>)session["ListaAluno"]).Count > 0)
-                {
-                    return;
-                }
+                session["ListaAluno"] = new List<Aluno>();
             }
-            var lista = new List<Aluno>();
-            lista.Add(new Aluno { Nome = "João", RA = "230211", Nascimento = new DateTime(2007, 2, 17) });
-            lista.Add(new Aluno { Nome = "Maria", RA = "230212", Nascimento = new DateTime(2006, 5, 12) });
-            lista.Add(new Aluno { Nome = "Carlos", RA = "230213", Nascimento = new DateTime(2007, 8, 25) });
-            lista.Add(new Aluno { Nome = "Ana", RA = "230214", Nascimento = new DateTime(2006, 3, 9) });
-            lista.Add(new Aluno { Nome = "Pedro", RA = "230215", Nascimento = new DateTime(2007, 11, 30) });
-            lista.Add(new Aluno { Nome = "Fernanda", RA = "230216", Nascimento = new DateTime(2006, 7, 19) });
-            lista.Add(new Aluno { Nome = "Lucas", RA = "230217", Nascimento = new DateTime(2007, 1, 5) });
-            lista.Add(new Aluno { Nome = "Juliana", RA = "230218", Nascimento = new DateTime(2006, 9, 14) });
-            lista.Add(new Aluno { Nome = "Gustavo", RA = "230219", Nascimento = new DateTime(2007, 6, 22) });
-            lista.Add(new Aluno { Nome = "Camila", RA = "230220", Nascimento = new DateTime(2006, 12, 3) });
-            lista.Add(new Aluno { Nome = "Rafael", RA = "230221", Nascimento = new DateTime(2007, 4, 8) });
-            lista.Add(new Aluno { Nome = "Beatriz", RA = "230222", Nascimento = new DateTime(2006, 10, 27) });
-            lista.Add(new Aluno { Nome = "André", RA = "230223", Nascimento = new DateTime(2007, 5, 15) });
-            lista.Add(new Aluno { Nome = "Letícia", RA = "230224", Nascimento = new DateTime(2006, 8, 11) });
-            lista.Add(new Aluno { Nome = "Eduardo", RA = "230225", Nascimento = new DateTime(2007, 2, 2) });
+            var lista = new List<Aluno>
+            { 
+                new Aluno { Id = 1, Nome = "João", Email = "joao@email.com", RA = "230211", Nascimento = new DateTime(2007, 2, 17) },
+                new Aluno { Id = 2, Nome = "Maria", Email = "maria@email.com", RA = "230212", Nascimento = new DateTime(2006, 5, 12) },
+                new Aluno { Id = 3, Nome = "Carlos", Email = "carlos@email.com", RA = "230213", Nascimento = new DateTime(2007, 8, 25) },
+                new Aluno { Id = 4, Nome = "Ana", Email = "ana@email.com", RA = "230214", Nascimento = new DateTime(2006, 3, 9) },
+                new Aluno { Id = 5, Nome = "Pedro", Email = "pedro@email.com", RA = "230215", Nascimento = new DateTime(2007, 11, 30) },
+                new Aluno { Id = 6, Nome = "Fernanda", Email = "fernanda@email.com", RA = "230216", Nascimento = new DateTime(2006, 7, 19) },
+                new Aluno { Id = 7, Nome = "Lucas", Email = "lucas@email.com", RA = "230217", Nascimento = new DateTime(2007, 1, 5) },
+                new Aluno { Id = 8, Nome = "Juliana", Email = "juliana@email.com", RA = "230218", Nascimento = new DateTime(2006, 9, 14) },
+                new Aluno { Id = 9, Nome = "Gustavo", Email = "gustavo@email.com", RA = "230219", Nascimento = new DateTime(2007, 6, 22) },
+                new Aluno { Id = 10, Nome = "Camila", Email = "camila@email.com", RA = "230220", Nascimento = new DateTime(2006, 12, 3) },
+                new Aluno { Id = 11, Nome = "Rafael", Email = "rafael@email.com", RA = "230221", Nascimento = new DateTime(2007, 4, 8) },
+                new Aluno { Id = 12, Nome = "Beatriz", Email = "beatriz@email.com", RA = "230222", Nascimento = new DateTime(2006, 10, 27) },
+                new Aluno { Id = 13, Nome = "André", Email = "andre@email.com", RA = "230223", Nascimento = new DateTime(2007, 5, 15) },
+                new Aluno { Id = 14, Nome = "Letícia", Email = "leticia@email.com", RA = "230224", Nascimento = new DateTime(2006, 8, 11) },
+                new Aluno { Id = 15, Nome = "Eduardo", Email = "eduardo@email.com", RA = "230225", Nascimento = new DateTime(2007, 2, 2) }
+            };
 
-            session.Remove("ListaAluno");
+
+        session.Remove("ListaAluno");
             session.Add("ListaAluno", lista);
         }
 
         public void Adicionar(HttpSessionStateBase session)
         {
-            if (session["ListaAluno"] != null)
-            {
-                (session["ListaAluno"] as List<Aluno>).Add(this);
-            }
+            var lista = session["ListaAluno"] as List<Aluno>;
+            this.Id = lista.Count;
+            lista.Add(this);
         }
 
         public static Aluno Procurar(HttpSessionStateBase session, int id)
         {
-            if (session["ListaAluno"] != null)
-            {
-                return (session["ListaAluno"] as List<Aluno>).ElementAt(id);
-            }
-
-            return null;
+            var lista = session["ListaAluno"] as List<Aluno>;
+            return lista.FirstOrDefault(a =>  a.Id == id);
         }
 
         public void Excluir(HttpSessionStateBase session)
         {
-
-            if (session["ListaAluno"] != null)
-            {
-                (session["ListaAluno"] as List<Aluno>).Remove(this);
-            }
+            var lista = session["ListaAluno"] as List<Aluno>;
+            lista.RemoveAll(a => a.Id == this.Id);
         }
 
         public void Editar(HttpSessionStateBase session, int id)
         {
-            if (session["ListaAluno"] != null)
+            var lista = session["ListaAluno"] as List<Aluno>;
+            var original = lista.FirstOrDefault(a => a.Id == id);
+            if (original != null) 
             {
-                var aluno = Aluno.Procurar(session, id);
-                aluno.Nome = this.Nome;
-                aluno.RA = this.RA;
-                aluno.Nascimento = this.Nascimento;
+                original.Nome = this.Nome;
+                original.RA = this.RA;
+                original.Nascimento = this.Nascimento;
+                original.Email = this.Email;
             }
         }
     }
